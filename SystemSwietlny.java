f49f31fb43f59b71aa69a0e78c25c6a38d144bcf
Arraos <m33how.cs@gmail.com>
2017-12-25 16:40:12 +0100

    Update SystemSwietlny.java

--- SystemSwietlny.java
+++ SystemSwietlny.java
@@ -1,5 +1,7 @@
 package Paczka;
 
+import java.util.ArrayList;
+
 public class SystemSwietlny {
 
     public static void main(String[] args) throws InterruptedException {
@@ -7,28 +9,38 @@ public class SystemSwietlny {
         Sygnalizator sygnalizator2 = new Sygnalizator();
         Sygnalizator sygnalizator3 = new Sygnalizator();
         Sygnalizator sygnalizator4 = new Sygnalizator();
-        sygnalizator1.id = 1;
-        sygnalizator2.id = 2;
-        sygnalizator3.id = 3;
-        sygnalizator4.id = 4;
-        sygnalizator1.grupa = 1;
-        sygnalizator2.grupa = 1;
-        sygnalizator3.grupa = 2;
-        sygnalizator4.grupa = 2;
         sygnalizator1.czasSwiecenia = 10000;
         sygnalizator2.czasSwiecenia = 10000;
         sygnalizator3.czasSwiecenia = 7000;
         sygnalizator4.czasSwiecenia = 7000;
-
-
+        ArrayList<Object> Grupa1 = new ArrayList<>();
+        Grupa1.add(sygnalizator1);
+        Grupa1.add(sygnalizator2);
+        ArrayList<Object> Grupa2 = new ArrayList<>();
+        Grupa2.add(sygnalizator3);
+        Grupa2.add(sygnalizator4);
+        int i = 0;
+        for(i = 0; i < Grupa1.size(); i++)
+        {
+            System.out.println(Grupa1.get(i));
+            Grupa1.indexOf(i)();
+        }
+        System.out.println();
         sygnalizator1.wlaczZielone();
-        if (sygnalizator1.wywolajNastepnySygnalizator() == Sygnalizator.class.getModifiers());
-            ;
-        int grupa=sygnalizator1.zwrocGrupe();
-        System.out.println(grupa);
-        sygnalizator1.pobierzGrupe(grupa);
+        System.out.println("zakonczone");
 
 
-        System.out.println("Zmiana swiatla dobiegla konca");
+
+
+
+       // sygnalizator1.wlaczZielone();
+        //if (sygnalizator1.wywolajNastepnySygnalizator() == Sygnalizator.class.getModifiers());
+        //;
+        //int grupa=sygnalizator1.zwrocGrupe();
+        //System.out.println(grupa);
+        //sygnalizator1.pobierzGrupe(grupa);
+
+
+        //System.out.println("Zmiana swiatla dobiegla konca");
     }
 }
