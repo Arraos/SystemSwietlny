93816409a462d17697261d02f714a9477d9fad44
Arraos <m33how.cs@gmail.com>
2017-12-25 16:40:34 +0100

    Update Sygnalizator.java

--- Sygnalizator.java
+++ Sygnalizator.java
@@ -1,87 +1,40 @@
 package Paczka;
 
 public class Sygnalizator {
-    int id;
-    Status swiatloZielone = Status.wylaczone;
-    Status swiatloZolte = Status.wylaczone;
-    Status swiatloCzerwone = Status.wylaczone;
-    int grupa;
+    Status swiatlo = Status.czerwone;
     int czasSwiecenia;
 
 
-    public int getGrupa() {        //Sprawdzenie numeru grupy
-        return grupa;
-    }
-    public int getId(){
-        return id;
-    }
     public int getCzasSwiecenia(){
         return czasSwiecenia;
     }
     //Rozpoczęcie bloku metod zmiany kazdego pojedynczego swiatla.
 
 
-    public void wlaczZielone(){
-        getGrupa();
-        int numerGrupy = getGrupa();
-        System.out.println("Numer grupy "+numerGrupy);
-        swiatloZielone = Status.wlaczone;
+    public void wlaczZielone() throws InterruptedException {
+        swiatlo = Status.zielone;
+        int czas = getCzasSwiecenia();
+        Thread.sleep(czas);
     }
-    public void wylaczZielone(){
-        getGrupa();
-        int numerGrupy = getGrupa();
-        System.out.println(numerGrupy);
-        swiatloZielone = Status.wylaczone;
+    public void wlaczZolte() throws InterruptedException {
+        swiatlo = Status.zolte;
+        int czas = getCzasSwiecenia();
+        Thread.sleep(czas);
     }
-    public void wlaczZolte(){
-        getGrupa();
-        int numerGrupy = getGrupa();
-        System.out.println(numerGrupy);
-        swiatloZolte = Status.wylaczone;
+    public void wlaczCzerwone() throws InterruptedException {
+        swiatlo = Status.czerwone;
+        int czas = getCzasSwiecenia();
+        Thread.sleep(czas);
     }
-    public void wylaczZolte(){
-        getGrupa();
-        int numerGrupy = getGrupa();
-        System.out.println(numerGrupy);
-        swiatloZolte = Status.wylaczone;
+    public void wlaczZoltoCzerwone() throws InterruptedException {
+        swiatlo = Status.zoltoczerwone;
+        int czas = getCzasSwiecenia();
+        Thread.sleep(czas);
     }
-    public void wlaczCzerwone(){
-        getGrupa();
-        int numerGrupy = getGrupa();
-        System.out.println(numerGrupy);
-        swiatloCzerwone = Status.wlaczone;
-    }
-    public void wylaczCzerwone(){
-        getGrupa();
-        int numerGrupy = getGrupa();
-        System.out.println(numerGrupy);
-        swiatloCzerwone = Status.wylaczone;
-    }
+
 
     //Koniec bloku metod zmiany każdego pojedynczego swiatla.
 
-//Operacja przekazania informacji do klasy Sygnalizator
-    int zwrocGrupe(){
-        return grupa;
-    }
-    boolean pobierzGrupe(int grupa){
-        if(grupa == zwrocGrupe()){
-            System.out.println("Prawda");
-            return true;
-        }
-        else{return false;}
-    }
-
-    public int wywolajNastepnySygnalizator(){
-       int NumerNastepnegoSygnalizatora = Sygnalizator.this.id+1;
-        return NumerNastepnegoSygnalizatora;
-    }
-    public int wywolajKolejnaGrupe(){
-       int NumerGrupy = Sygnalizator.this.grupa+1;
-        System.out.println(Sygnalizator.this.grupa);
-        return NumerGrupy;
-    }
 
 
 }
-
