222939781cb82dcf52a9bc9030955dcb77dd8fcb
Arraos <m33how.cs@gmail.com>
2017-12-26 18:04:06 +0100

    Trzecia wersja

--- SystemSwietlny.java
+++ SystemSwietlny.java
@@ -12,11 +12,8 @@ public class SystemSwietlny {
         Sygnalizator sygnalizator2 = new Sygnalizator();
         Sygnalizator sygnalizator3 = new Sygnalizator();
         Sygnalizator sygnalizator4 = new Sygnalizator();
+        Licznik Sekundomierz = new Licznik();
         //Deklaracja wartosci konstruktorow
-        sygnalizator1.czasSwiecenia = 10000;
-        sygnalizator2.czasSwiecenia = 10000;
-        sygnalizator3.czasSwiecenia = 7000;
-        sygnalizator4.czasSwiecenia = 7000;
         System.out.println(sygnalizator1.swiatlo);
         System.out.println(sygnalizator2.swiatlo);
         // Tworzenie listy z obiektami gdzie "lista1" to sygnalizatory poziome, a "lista2" to sygnalizatory pionowe
@@ -27,18 +24,68 @@ public class SystemSwietlny {
         //Deklaracja licznikow
         int o = 0;
         int i = 0;
-        //Warunek if z petla for
-            for (;i < lista1.length; i++)
-                {
-                lista1[i].wlaczZielone();
-                //Sprawdzenie poprawnosci
-                System.out.println(sygnalizator1.swiatlo);
-                System.out.println(sygnalizator2.swiatlo);
-                o++;
-                }
-            int czas = lista1[i-1].getCzasSwiecenia();
-            Thread.sleep(czas);
-            System.out.println("Nastapila zmiana swiatla");
+        //Warunek while
+            //Pętle for dla listy pierwszej
+            for (Sygnalizator sekwencja: lista1) {
+                sekwencja.wlaczZoltoCzerwone();
+                System.out.println(sekwencja.swiatlo);
+            }
+
+            Thread.sleep(Sekundomierz.getCzasZoltoZielone());
+
+            for(Sygnalizator sekwencja : lista1) {
+                 sekwencja.wlaczZielone();
+                 System.out.println(sekwencja.swiatlo);
+             }
+             Thread.sleep(Sekundomierz.getCzasZielone());
+
+             for (Sygnalizator sekwencja: lista1)
+             {
+                 sekwencja.wlaczZolte();
+                 System.out.println(sekwencja.swiatlo);
+            }
+
+             Thread.sleep(Sekundomierz.getCzasZolte());
+
+            for (Sygnalizator sekwencja: lista1) {
+                sekwencja.wlaczCzerwone();
+                System.out.println(sekwencja.swiatlo);
+            }
+
+            Thread.sleep(Sekundomierz.getCzasCzerwone());
+
+            //Pętle for dla listy drugiej
+
+            for (Sygnalizator sekwencja: lista2)
+            {
+                sekwencja.wlaczZoltoCzerwone();
+                System.out.println(sekwencja.swiatlo);
+            }
+
+            Thread.sleep(Sekundomierz.getCzasZoltoZielone());
+
+            for (Sygnalizator sekwencja: lista2)
+            {
+                sekwencja.wlaczZielone();
+                System.out.println(sekwencja.swiatlo);
+            }
+
+            Thread.sleep(Sekundomierz.getCzasZielone());
+
+            for (Sygnalizator sekwencja: lista2) {
+                sekwencja.wlaczZolte();
+                System.out.println(sekwencja.swiatlo);
+            }
+
+            Thread.sleep(Sekundomierz.getCzasZolte());
+
+            for (Sygnalizator sekwencja: lista2) {
+                sekwencja.wlaczCzerwone();
+                System.out.println(sekwencja.swiatlo);
+            }
+
+            Thread.sleep(Sekundomierz.getCzasCzerwone());
+            
         System.out.println("GIT");
 
     }
