5d377dacf1a72921c08299b3f627c24a185ec99b
Arraos <m33how.cs@gmail.com>
2017-12-26 18:05:50 +0100

    Licznik obsługujący czas świecenia oraz aktualny czas

---
+++ Licznik.java
@@ -0,0 +1,22 @@
+package Paczka;
+
+public class Licznik {
+    int czasZielone = 20000;
+    int czasZolte = 10000;
+    int czasZoltoZielone = 5000;
+    int czasCzerwone = 5000;
+
+    public int getCzasZielone(){
+    return czasZielone;
+}
+    public int getCzasZolte(){
+        return czasZolte;
+    }
+    public int getCzasZoltoZielone(){
+        return czasZoltoZielone;
+    }
+    public int getCzasCzerwone()
+    {
+        return czasCzerwone;
+    }
+}
