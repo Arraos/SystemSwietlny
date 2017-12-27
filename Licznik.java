9d6c2e92d6cb0cc50655161f911e74fc749adb88
Arraos <m33how.cs@gmail.com>
2017-12-27 16:24:11 +0100

    Update Metod

--- Licznik.java
+++ Licznik.java
@@ -1,5 +1,8 @@
 package Paczka;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Licznik {
     int czasZielone = 20000;
     int czasZolte = 10000;
@@ -18,5 +21,25 @@ public class Licznik {
     public int getCzasCzerwone()
     {
         return czasCzerwone;
+    }
+    public void setCzasZielone(int czasTeraz) {
+        if (czasTeraz == 1) {       // Od 6:30 do 9:00 oraz od 15:00 do 18:00
+            czasZielone = 40000;
+        }
+        else if(czasTeraz == 2){    // Od 9:00 do 15:00 oraz od 18:00 do 23:00
+            czasZielone = 30000;
+        }
+        else if (czasTeraz == 3){   // Od 23:00 do 6:30
+            czasZielone = 70000;
+        }
+    }
+    public int czasTeraz()
+    {
+        Date currentDate = new Date();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+        String dateString = dateFormat.format(currentDate);
+        System.out.println(dateString);
+
+        if (dateString<12)
     }
 }
