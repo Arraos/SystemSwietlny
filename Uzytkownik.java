070d495f6efef9d640e4327f5b4387e801f374ea
Arraos <m33how.cs@gmail.com>
2017-12-30 13:50:10 +0100

    Create Uzytkownik.java

---
+++ Uzytkownik.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Uzytkownik{
+    public void SkrocSekwencje()
+    {
+
+    }
+    public class AdministratorSieci extends Uzytkownik{
+        public void IleSygnalizatorow() {
+        System.out.println();
+        }
+        public void DodajSygnalizator(){
+
+        }
+    }
+}
